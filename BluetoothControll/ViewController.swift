26c069234587a7860fb82a7428ba46b78e48dda9
Mazdah <woohj70@gmail.com>
2016-06-17 15:45:15 +0900

    Initial Commit

---
+++ BluetoothControll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluetoothControll
+//
+//  Created by Mazdah on 2016. 6. 17..
+//  Copyright © 2016년 mazdah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
