32f6f2f002939725567d0e99868d7e9db538fcb4
Mazdah <woohj70@gmail.com>
2016-06-23 18:33:19 +0900

    통신 프로토콜 정의 시작

--- BluetoothControll/Definitions.swift
+++ BluetoothControll/Definitions.swift
@@ -14,3 +14,10 @@ let NOTIFY_MTU = 20
 
 let transferServiceUUID = CBUUID(string: TRANSFER_SERVICE_UUID)
 let transferCharacteristicUUID = CBUUID(string: TRANSFER_CHARACTERISTIC_UUID)
+
+
+// protocall define
+
+let R_CONNECT = 0x80 // 로봇에게 연결 여부를 물음
+let R_READY = 0x81   // 로봇에게 기동 준비를 시킴 로봇으로부터 0x82를 돌려받음
+let R_CLOSE = 0xFF // 로봇의 기동을 중지시키고 자세를 초기화 함
